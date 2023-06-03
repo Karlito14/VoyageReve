7d2d05abfcb1bd20f87a1c039adc9e3bd986d291
Karlito14 <leiroz26@hotmail.com>
2023-06-03 15:45:35 +0200

    mise en place de l'environnement

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+    watch: true,
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    devtool: 'source-map',
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        open: true
+    }
+}
